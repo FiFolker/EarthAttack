1f4b7efef71a9cdc4e9e8cfb1c2787fddce85304
Callixte Muller <callixte.muller@etu.u-bordeaux.fr>
2023-02-01 11:33:34 +0100

    Ajout fonction clock et javadoc

--- EarthAttack/src/earthattack/EarthAttack.java
+++ EarthAttack/src/earthattack/EarthAttack.java
@@ -19,6 +19,12 @@ public class EarthAttack {
        
     }
     
+    
+    /**
+     * Simule un chargement avant la prochaine question si la précèdent était juste.
+     * @param result si la question a été réussi ou pas.
+     * @throws InterruptedException 
+     */
     static void loadingNextAnswer(boolean result) throws InterruptedException{
         if (result){
             System.out.println("Sending new code...");
@@ -31,4 +37,21 @@ public class EarthAttack {
         }
     }
 
+    /**
+     * Calcule le temps qui s'écoule. 
+     * @param run booléen qui est à faux si toute les réponses ont été répondus, vrai
+     * sinon.
+     * @return le temps écoulé en secondes. 
+     */
+    static double clock(boolean run){
+        double time = 0;
+        double startTime = System.nanoTime() / 10e9;
+        double currentTime;
+        while (time <= 1200 & run){
+            currentTime = System.nanoTime();
+            time =  currentTime - startTime;
+        }
+        return time;
+    }
+    
 }
