fb468fb5db4ba74f075fac00c3ff6789779a9cd1
Le Merle Elouan <65813059+FiFolker@users.noreply.github.com>
2023-02-03 18:45:56 +0100

    copy paste of 737e0e75 commit

--- EarthAttack/src/earthattack/User.java
+++ EarthAttack/src/earthattack/User.java
@@ -2,9 +2,11 @@ package earthattack;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -15,70 +17,80 @@ import java.util.logging.Logger;
  */
 public class User {
 
-    double score;
-    String name;
-    static ArrayList<User> users = new ArrayList<>();
+	double score;
+	String name;
+	static HashSet<User> users = new HashSet<>();
 
-    User() {
-        this.name = "";
-        this.score = 0.;
-        users.add(this);
-    }
+	User() {
+		this.name = "";
+		this.score = 0.;
+		users.add(this);
+	}
 
-    User(String nName, double nScore) {
-        this.name = nName;
-        this.score = nScore;
-    }
+	User(String nName, double nScore) {
+		this.name = nName;
+		this.score = nScore;
+	}
 
-    static void initialiseUsers(){
-        try(Scanner file = new Scanner(new File(EarthAttack.FILES[3]))){
-           while(file.hasNextLine()){
-               String line = file.next();
-               line.split(";");
-               
-               User user = null;
-               users.add(user);
-           } 
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    static boolean validUser(String name) {
-        boolean userNotFound = true;
-        int i = 0;
-        while (userNotFound & i < users.size()) {
-            User cUser = users.get(i);
-            userNotFound = (cUser.name != name) & (!cUser.name.isEmpty());
-        }
+	public static void main(String[] args) {
+		User.initialiseUsers();
+		System.out.println(User.users.toString());
+	}
 
-        return userNotFound;
-    }
-
-    static void flushUsers() {
-        try (PrintWriter fileLeaderboard = new PrintWriter(new File(EarthAttack.FILES[3]));){
-			
-			for(User u : users){
-				fileLeaderboard.print(u.name +","+ u.score + ";");
+	static void initialiseUsers() {
+		try (Scanner file = new Scanner(new File(EarthAttack.FILES[3]))) {
+			while (file.hasNextLine()) {
+				String line = file.next();
+				String[] userScoreTogether = line.split(";");
+				System.out.println(userScoreTogether.length);
+				for (int i = 0; i < userScoreTogether.length - 1; i++) {
+					String[][] userInfo = new String[userScoreTogether.length][2];
+					for (int j = 0; j < 2; i++) {
+						userInfo[i][j] = userScoreTogether[i].split(",")[j];
+					}
+					User user = new User(userInfo[i][0], Integer.parseInt(userInfo[i][1]));
+					users.add(user);
+				}
 			}
-			
+		} catch (FileNotFoundException ex) {
+			Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	static boolean validUser(String name) {
+		// Could apply different rules to format names but it isn't needed YET
+		return users.contains(name) & !name.isEmpty();
+	}
+
+	static void flushUsers() {
+		try (PrintWriter fileLeaderboard = new PrintWriter(new File(EarthAttack.FILES[3]));) {
+
+			for (User u : users) {
+				fileLeaderboard.print(u.name + "," + u.score + ";");
+			}
+
 		} catch (FileNotFoundException ex) {
 			System.out.println("Ouverture du fichier impossible : " + ex);
 		} catch (IOException ex) {
 			System.out.println("Erreur d'écriture : " + ex);
 		}
 
-    }
-    
-    static User userSelect() {
-        Scanner sc = new Scanner(System.in);
-        String name = "";
-        do{
-            System.out.println("Saisisez un pseudonyme.");
-            name = sc.next();
-        } while (!User.validUser(name));
+	}
 
-        return new User(name, 0);
-    }
+	static User userSelect() {
+		Scanner sc = new Scanner(System.in);
+		String name = "";
+		do {
+			System.out.println("Saisisez un pseudonyme.");
+			name = sc.next();
+		} while (User.validUser(name));
+
+		return new User(name, 0);
+	}
 
 }
+
+
+	
+	
+
